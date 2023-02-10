621b66a93acf77ae7244ff81fad07e1ba2042839
Marimeko <98724873+Marimeko@users.noreply.github.com>
2023-02-10 13:41:04 +0000

    pch_dma: Use GFP_ATOMIC because called from interrupt context

--- test/lib/test_matasano.rb
+++ test/lib/test_matasano.rb
@@ -1,1 +1,10 @@
-require_relative "../../
+require_relative "../../lib/matasano"
+require "minitest/autorun"
+require 'securerandom'
+
+class TestMatasano < Minitest::Test
+  def setup
+  end
+
+  def test_str_to_bytes
+    assert_equal [65, 66,
