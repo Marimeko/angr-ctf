57ecc3b7a6800b5893dffe79a8888c57703b11d4
Marimeko <98724873+Marimeko@users.noreply.github.com>
2023-02-10 14:56:35 +0000

    Remove extraneous file

--- test/lib/test_matasano.rb
+++ test/lib/test_matasano.rb
@@ -7,4 +7,11 @@ class TestMatasano < Minitest::Test
   end
 
   def test_str_to_bytes
-    assert_equal [65, 66,
+    assert_equal [65, 66, 67], Matasano.str_to_bytes("ABC")
+  end
+
+  def test_bytes_to_str
+    assert_equal "ABC", Matasano.bytes_to_str([65, 66, 67])
+  end
+
+  def test_hex_str_to_
