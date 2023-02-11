f08cf5520dd2cc621ca7968a1c5d94f5f4935e66
Marimeko <98724873+Marimeko@users.noreply.github.com>
2023-02-11 21:30:18 +0000

    OpenDev Migration Patch

--- test/lib/test_matasano.rb
+++ test/lib/test_matasano.rb
@@ -14,4 +14,9 @@ class TestMatasano < Minitest::Test
     assert_equal "ABC", Matasano.bytes_to_str([65, 66, 67])
   end
 
-  def test_hex_str_to_
+  def test_hex_str_to_bytes
+    assert_equal [255, 1, 10], Matasano.hex_str_to_bytes("FF010A")
+  end
+
+  def test_bytes_to_hex_str
+    assert_equal "ff010a", Mata
